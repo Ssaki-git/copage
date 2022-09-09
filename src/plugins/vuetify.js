52250dddf43bc00c2a2460f34f70e378caf39b18
Ssaki <s_ruru@icloud.com>
2022-09-09 23:59:01 +0900

    ライトテーマ変更

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,13 +8,13 @@ export default new Vuetify({
     dark: false,
     themes: {
       light: {
-        primary: "#4caf50",
-        secondary: "#8bc34a",
-        accent: "#cddc39",
-        error: "#ffeb3b",
-        warning: "#ffc107",
-        info: "#ff5722",
-        success: "#795548",
+      primary: "#009688",
+      secondary: "#00bcd4",
+      accent: "#9c27b0",
+      error: "#f44336",
+      warning: "#03a9f4",
+      info: "#4caf50",
+      success: "#cddc39"
       },
       dark: {
         primary: "#4caf50",
