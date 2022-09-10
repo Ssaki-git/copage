d70bd117b6a67dcca76ebd057ccd2619d139e838
Ssaki <s_ruru@icloud.com>
2022-09-10 20:00:08 +0900

    toppageslider作成

--- src/components/HomeAboutslider.vue
+++ src/components/HomeAboutslider.vue
@@ -5,20 +5,23 @@
     </div>
     <!-- ここから追加 -->
     <v-container>
-      <v-row class="home-slide__contents">
-        <v-col md="5" class="home-slide__contents-img">
-          <img src="../assets/port01.png" alt="" class="img-fluid" />
-        </v-col>
-        <v-col md="5" class="home-slide__contents-text">
-          <p>
-            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-          </p>
-          <p>
-            XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-          </p>
-        </v-col>
-      </v-row>
-      <AppButton :button-text="buttonText" :url="url" />
+      <v-sheet>
+        <v-slide-group multiple show-arrows>
+          <v-slide-item v-for="n in 8" :key="n">
+            <v-card style="margin: 0 20px 0 0; width: 320px; height: 240px">
+              <v-img
+                class="white--text align-end"
+                height="120px"
+                src="../assets/port01.png"
+              />
+              <v-card-title>sample title {{ n }}</v-card-title>
+              <v-card-text class="text--primary">
+                {{ n }}番目のカードテキスト
+              </v-card-text>
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
     </v-container>
     <!-- ここまで追加 -->
   </section>
