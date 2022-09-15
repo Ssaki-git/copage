fc0d74c9151a4d72811f111fe5c0d520bc5e2ccc
Ssaki <s_ruru@icloud.com>
2022-09-16 00:00:39 +0900

    サービス　ダイアログ画像、テキスト変更

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -8,17 +8,16 @@
             <v-dialog v-model="dialog">
               <v-card>
                 <v-card-title class="headline grey lighten-2">
-                  Vuetify で v-dialog をつかってみる
+                  業務内容１
                 </v-card-title>
+                <v-img
+                  class="white--text align-end"
+                  height="500px"
+                  src="../assets/aboutco.png"
+                />
                 <v-card-text style="margin: 1rem auto">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
                 </v-card-text>
                 <v-divider></v-divider>
                 <v-card-actions>
@@ -41,7 +40,7 @@
             </div>
             <div class="service__card-text">
               <p>
-                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                ダイアログが開きますXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
               </p>
             </div>
           </v-card>
